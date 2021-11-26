72873c9f61208762d242d549c4f9436149c6b501
Jody <qizhaodi@koderover.com>
2021-11-26 15:58:06 +0800

    define download name
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/delivery/version/helm/detail.vue
+++ src/components/projects/delivery/version/helm/detail.vue
@@ -81,7 +81,7 @@
               <el-table-column label="操作">
                 <template slot-scope="{row}">
                   <!-- <el-button type="text">预览</el-button> -->
-                  <a download :href="downloadChartUrl(row)">
+                  <a :download="`${row.chartName}-${row.chartVersion}`" :href="downloadChartUrl(row)">
                     <el-button type="text">下载</el-button>
                   </a>
                 </template>

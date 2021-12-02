fd33ade45d415ae88f1f79d4e27a17c54a5622de
Jody <qizhaodi@koderover.com>
2021-12-02 15:23:35 +0800

    update env show
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/env/env_detail/env_detail_comp.vue
+++ src/components/projects/env/env_detail/env_detail_comp.vue
@@ -60,12 +60,12 @@
                 <div class="grid-content">所属集群:</div>
               </el-col>
               <el-col :span="8">
-                <div v-if="productInfo.cluster_id"
+                <div v-if="productInfo.is_local"
+                     class="grid-content">{{productInfo.cluster_name}}(本地集群)</div>
+                <div v-else
                      class="grid-content">
                   {{productInfo.is_prod?productInfo.cluster_name+' (生产集群)':productInfo.cluster_name +' (测试集群)'}}
                 </div>
-                <div v-else
-                     class="grid-content">{{productInfo.cluster_name}}(本地集群)</div>
               </el-col>
             </template>
             <el-col :span="3">

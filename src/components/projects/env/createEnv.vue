031befcc992be6c0123f2592e11ec678a32f28d4
Jody <qizhaodi@koderover.com>
2021-12-09 16:00:40 +0800

    init
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/env/createEnv.vue
+++ src/components/projects/env/createEnv.vue
@@ -4,14 +4,11 @@
 <script>
 import CreatHostEnv from './hostEnv/creatHostEnv.vue'
 import CreatEnvDetail from './inner_env/create_env_detail.vue'
+import CreateHelmEnv from './createHelmEnv.vue'
 import { getSingleProjectAPI } from '@/api'
 
 export default {
   name: 'createEnv',
-  components: {
-    CreatEnvDetail,
-    CreatHostEnv
-  },
   data () {
     return {
       currentComponents: null
@@ -21,9 +18,12 @@ export default {
     async checkProjectFeature () {
       const projectName = this.$route.params.project_name
       this.projectInfo = await getSingleProjectAPI(projectName)
-      if (this.projectInfo.product_feature) {
-        if (this.projectInfo.product_feature.create_env_type === 'external') {
+      const feature = this.projectInfo.product_feature
+      if (feature) {
+        if (feature.create_env_type === 'external') {
           this.currentComponents = CreatHostEnv
+        } else if (feature.deploy_type === 'helm') {
+          this.currentComponents = CreateHelmEnv
         } else {
           this.currentComponents = CreatEnvDetail
         }// Compatible with old project

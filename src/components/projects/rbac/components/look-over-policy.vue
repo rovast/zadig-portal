37160b632cd955c5e67b44a67e9d1eb5dfac7c33
Jody <qizhaodi@koderover.com>
2021-12-23 14:25:28 +0800

    feat(pocily): policy html css
    Signed-off-by: Jody <qizhaodi@koderover.com>

---
+++ src/components/projects/rbac/components/look-over-policy.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-dialog title="策略详情" :visible.sync="policyDialog" width="50%">
+    <section class="policy-content">
+      <div>
+        <span class="info-title">策略名称</span>
+        <span>{{}}</span>
+      </div>
+      <div>
+        <span class="info-title">描述信息</span>
+        <span>{{}}</span>
+      </div>
+      <el-table :data="policyInfo">
+        <el-table-column prop="prop" label="模块名称"></el-table-column>
+        <el-table-column prop="prop" label="权限项"></el-table-column>
+        <el-table-column prop="prop" label="过滤规则"></el-table-column>
+        <el-table-column prop="prop" label="关联资源"></el-table-column>
+      </el-table>
+    </section>
+    <div slot="footer">
+      <el-button type="primary" @click="policyDialog = false" size="small">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialogFlag: Boolean,
+    policyInfo: Object
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    policyDialog: {
+      get () {
+        return this.dialogFlag
+      },
+      set (val) {
+        this.$emit('update:policyDialog', val)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+/deep/.el-dialog__body {
+  padding-top: 10px;
+
+  .policy-content {
+    color: #777;
+
+    .info-title {
+      display: inline-block;
+      margin-right: 10px;
+      font-weight: 500;
+      line-height: 2;
+    }
+  }
+}
+</style>
--- src/components/projects/rbac/policy.vue
+++ src/components/projects/rbac/policy.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="rbac-policy">
-    policy
+    <el-alert type="info" :closable="false" description="用户与策略关联后，即可获得策略描述的权限"></el-alert>
+
+    <el-table :data="policies" style="width: 100%;">
+      <el-table-column prop="prop" label="策略名称"></el-table-column>
+      <el-table-column prop="prop" label="描述信息"></el-table-column>
+      <el-table-column prop="prop" label="修改时间">
+          <template slot-scope="{ row }">
+              <span>{{ $utils.convertTimestamp(row.time) }}</span>
+          </template>
+      </el-table-column>
+      <el-table-column label="操作">
+          <template slot-scope="{ $index }">
+              <el-button type="primary" size="small" plain @click="lookOverPolicy($index)">查看</el-button>
+          </template>
+      </el-table-column>
+    </el-table>
+
+    <LookOverPolicy :dialogFlag.sync="dialogFlag" :policyInfo="policyInfo"></LookOverPolicy>
   </div>
 </template>
 
 <script>
+import LookOverPolicy from './components/look-over-policy.vue'
 export default {
   data () {
-    return {}
+    return {
+      policies: [],
+      dialogFlag: false,
+      policyInfo: {}
+    }
+  },
+  methods: {
+    lookOverPolicy (index) {
+      this.dialogFlag = true
+      console.log('look ', index)
+    }
+  },
+  components: {
+    LookOverPolicy
   }
 }
 </script>

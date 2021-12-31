d68ba1b91ffd2be8b235ad8084b8f310b3a26519
Jody <qizhaodi@koderover.com>
2021-12-31 15:08:51 +0800

    style： delete useless space
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/edit_pipeline/product_pipeline/switch_tab/notify.vue
+++ src/components/projects/edit_pipeline/product_pipeline/switch_tab/notify.vue
@@ -1,112 +1,112 @@
 <template>
-    <el-card class="notify box-card">
-        <div class="script dashed-container" v-if="showTitle">
-          <span class="title">通知</span>
-        </div>
-        <div class="notify dashed-container">
-          <div class="notify-container">
-            <el-form :model="notify"
-                     :rules="notifyRules"
-                     label-position="top"
-                     ref="notify">
-              <el-form-item prop="webhook_type">
-                <span slot="label">
-                  <span>Webhook 类型：</span>
-                </span>
-                <el-select v-model="notify.webhook_type"
-                           @change="clearForm"
-                           style="width: 350px;"
-                           size="small"
-                           placeholder="请选择类型">
-                  <el-option label="钉钉"
-                             value="dingding">
-                  </el-option>
-                  <el-option label="企业微信"
-                             value="wechat">
-                  </el-option>
-                  <el-option label="飞书"
-                             value="feishu">
-                  </el-option>
-                </el-select>
-              </el-form-item>
-              <el-form-item v-if="notify.webhook_type==='feishu'"
-                            prop="feishu_webhook">
-                <span slot="label">
-                  <span>Webhook 地址：
-                    <el-tooltip class="item"
-                                effect="dark"
-                                content="点击查看飞书 webhook 配置文档"
-                                placement="top">
-                      <a class="help-link"
-                         href="https://docs.koderover.com/zadig/project/workflow/#%E7%8A%B6%E6%80%81%E9%80%9A%E7%9F%A5"
-                         target="_blank"><i class="el-icon-question"></i></a>
-                    </el-tooltip></span>
-                </span>
-                <el-input style="width: 350px;"
-                          size="small"
-                          v-model="notify.feishu_webhook"></el-input>
-              </el-form-item>
-              <el-form-item v-if="notify.webhook_type==='wechat'"
-                            prop="weChat_webHook">
-                <span slot="label">
-                  <span>Webhook 地址：
-                    <el-tooltip class="item"
-                                effect="dark"
-                                content="点击查看企业微信 webhook 配置文档"
-                                placement="top">
-                      <a class="help-link"
-                         href="https://docs.koderover.com/zadig/project/workflow/#%E7%8A%B6%E6%80%81%E9%80%9A%E7%9F%A5"
-                         target="_blank"><i class="el-icon-question"></i></a>
-                    </el-tooltip></span>
-                </span>
-                <el-input style="width: 350px;"
-                          size="small"
-                          v-model="notify.weChat_webHook"></el-input>
-              </el-form-item>
-              <el-form-item v-if="notify.webhook_type==='dingding'"
-                            prop="dingding_webhook">
-                <span slot="label">
-                  <span>Webhook 地址：
-                    <el-tooltip class="item"
-                                effect="dark"
-                                content="点击查看钉钉 webhook 配置文档"
-                                placement="top">
-                      <a class="help-link"
-                         href="https://docs.koderover.com/zadig/project/workflow/#%E7%8A%B6%E6%80%81%E9%80%9A%E7%9F%A5"
-                         target="_blank"><i class="el-icon-question"></i></a>
-                    </el-tooltip></span>
-                </span>
-                <el-input style="width: 350px;"
-                          size="small"
-                          v-model="notify.dingding_webhook"></el-input>
-              </el-form-item>
-              <el-form-item v-if="notify.webhook_type==='dingding'"
-                            prop="at_mobiles">
-                <span slot="label">
-                  <span>@指定成员（输入指定通知接收人的手机号码，使用 ; 分割，为空则全员通知）：</span>
-                </span>
-                <el-input style="width: 350px;"
-                          type="textarea"
-                          :rows="3"
-                          placeholder="输入指定通知接收人的手机号码，使用用 ; 分割"
-                          v-model="mobileStr">
-                </el-input>
-              </el-form-item>
+  <el-card class="notify box-card">
+    <div class="script dashed-container" v-if="showTitle">
+      <span class="title">通知</span>
+    </div>
+    <div class="notify dashed-container">
+      <div class="notify-container">
+        <el-form :model="notify"
+                  :rules="notifyRules"
+                  label-position="top"
+                  ref="notify">
+          <el-form-item prop="webhook_type">
+            <span slot="label">
+              <span>Webhook 类型：</span>
+            </span>
+            <el-select v-model="notify.webhook_type"
+                        @change="clearForm"
+                        style="width: 350px;"
+                        size="small"
+                        placeholder="请选择类型">
+              <el-option label="钉钉"
+                          value="dingding">
+              </el-option>
+              <el-option label="企业微信"
+                          value="wechat">
+              </el-option>
+              <el-option label="飞书"
+                          value="feishu">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item v-if="notify.webhook_type==='feishu'"
+                        prop="feishu_webhook">
+            <span slot="label">
+              <span>Webhook 地址：
+                <el-tooltip class="item"
+                            effect="dark"
+                            content="点击查看飞书 webhook 配置文档"
+                            placement="top">
+                  <a class="help-link"
+                      href="https://docs.koderover.com/zadig/project/workflow/#%E7%8A%B6%E6%80%81%E9%80%9A%E7%9F%A5"
+                      target="_blank"><i class="el-icon-question"></i></a>
+                </el-tooltip></span>
+            </span>
+            <el-input style="width: 350px;"
+                      size="small"
+                      v-model="notify.feishu_webhook"></el-input>
+          </el-form-item>
+          <el-form-item v-if="notify.webhook_type==='wechat'"
+                        prop="weChat_webHook">
+            <span slot="label">
+              <span>Webhook 地址：
+                <el-tooltip class="item"
+                            effect="dark"
+                            content="点击查看企业微信 webhook 配置文档"
+                            placement="top">
+                  <a class="help-link"
+                      href="https://docs.koderover.com/zadig/project/workflow/#%E7%8A%B6%E6%80%81%E9%80%9A%E7%9F%A5"
+                      target="_blank"><i class="el-icon-question"></i></a>
+                </el-tooltip></span>
+            </span>
+            <el-input style="width: 350px;"
+                      size="small"
+                      v-model="notify.weChat_webHook"></el-input>
+          </el-form-item>
+          <el-form-item v-if="notify.webhook_type==='dingding'"
+                        prop="dingding_webhook">
+            <span slot="label">
+              <span>Webhook 地址：
+                <el-tooltip class="item"
+                            effect="dark"
+                            content="点击查看钉钉 webhook 配置文档"
+                            placement="top">
+                  <a class="help-link"
+                      href="https://docs.koderover.com/zadig/project/workflow/#%E7%8A%B6%E6%80%81%E9%80%9A%E7%9F%A5"
+                      target="_blank"><i class="el-icon-question"></i></a>
+                </el-tooltip></span>
+            </span>
+            <el-input style="width: 350px;"
+                      size="small"
+                      v-model="notify.dingding_webhook"></el-input>
+          </el-form-item>
+          <el-form-item v-if="notify.webhook_type==='dingding'"
+                        prop="at_mobiles">
+            <span slot="label">
+              <span>@指定成员（输入指定通知接收人的手机号码，使用 ; 分割，为空则全员通知）：</span>
+            </span>
+            <el-input style="width: 350px;"
+                      type="textarea"
+                      :rows="3"
+                      placeholder="输入指定通知接收人的手机号码，使用用 ; 分割"
+                      v-model="mobileStr">
+            </el-input>
+          </el-form-item>
 
-              <el-form-item prop="notify_type"
-                            label="通知事件：">
-                <el-checkbox :indeterminate="isIndeterminate"
-                             v-model="checkAll"
-                             @change="handleCheckAllChange">全选</el-checkbox>
-                <el-checkbox-group @change="handleCheckedValueChange"
-                                   v-model="notify.notify_type">
-                  <el-checkbox v-for="type in notifyType" :key="type.label" :label="type.label">{{type.desc}}</el-checkbox>
-                </el-checkbox-group>
-              </el-form-item>
-            </el-form>
-          </div>
-        </div>
-    </el-card>
+          <el-form-item prop="notify_type"
+                        label="通知事件：">
+            <el-checkbox :indeterminate="isIndeterminate"
+                          v-model="checkAll"
+                          @change="handleCheckAllChange">全选</el-checkbox>
+            <el-checkbox-group @change="handleCheckedValueChange"
+                                v-model="notify.notify_type">
+              <el-checkbox v-for="type in notifyType" :key="type.label" :label="type.label">{{type.desc}}</el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </el-card>
 </template>
 
 <script type="text/javascript">
@@ -260,51 +260,51 @@ export default {
 </script>
 
 <style lang="less">
-  .notify.box-card {
-    .el-card__header {
-      text-align: center;
-    }
+.notify.box-card {
+  .el-card__header {
+    text-align: center;
+  }
 
-    .el-form {
-      .el-form-item {
-        margin-bottom: 5px;
+  .el-form {
+    .el-form-item {
+      margin-bottom: 5px;
 
-        .env-form-inline {
-          width: 100%;
-        }
-      }
-    }
-
-    .divider {
-      width: 100%;
-      height: 1px;
-      margin: 13px 0;
-      background-color: #dfe0e6;
-    }
-
-    .notify-container {
-      margin: 10px 0;
-    }
-
-    .help-link {
-      color: #303133;
-    }
-
-    .script {
-      padding: 5px 0;
-
-      .title {
-        display: inline-block;
-        width: 100px;
-        padding-top: 6px;
-        color: #606266;
-        font-size: 14px;
-      }
-
-      .item-title {
-        margin-left: 5px;
-        color: #909399;
+      .env-form-inline {
+        width: 100%;
       }
     }
   }
+
+  .divider {
+    width: 100%;
+    height: 1px;
+    margin: 13px 0;
+    background-color: #dfe0e6;
+  }
+
+  .notify-container {
+    margin: 10px 0;
+  }
+
+  .help-link {
+    color: #303133;
+  }
+
+  .script {
+    padding: 5px 0;
+
+    .title {
+      display: inline-block;
+      width: 100px;
+      padding-top: 6px;
+      color: #606266;
+      font-size: 14px;
+    }
+
+    .item-title {
+      margin-left: 5px;
+      color: #909399;
+    }
+  }
+}
 </style>

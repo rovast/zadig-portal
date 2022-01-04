94e8d15e44f01cc8ef5e1a7778a7d84eb175bb89
fansi <fansiqiong@koderover.com>
2022-01-04 21:28:07 +0800

    fix document href 404
    Signed-off-by: fansi <fansiqiong@koderover.com>

--- src/components/setting/integration/other.vue
+++ src/components/setting/integration/other.vue
@@ -26,7 +26,7 @@
             <el-link
               style="font-size: 14px; vertical-align: baseline;"
               type="primary"
-              :href="`https://docs.koderover.com/zadig/settings/other/`"
+              :href="`https://docs.koderover.com/zadig/settings/others/`"
               :underline="false"
               target="_blank"
             >帮助文档</el-link>。

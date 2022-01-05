963a62c17efde540c0eb6cecb6993d02ab7224f3
Jody <qizhaodi@koderover.com>
2022-01-05 14:25:18 +0800

    fix: modify annotation and modify source
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/env/env_detail/env_detail_comp.vue
+++ src/components/projects/env/env_detail/env_detail_comp.vue
@@ -92,7 +92,8 @@
               </div>
             </el-col>
           </el-row>
-          <el-row :gutter="20" v-if="!pmServiceList.length">
+          <!-- host project don't show registry -->
+          <el-row :gutter="20" v-if="!isPmService">
             <el-col :span="3">
               <div class="grid-content">镜像仓库:</div>
             </el-col>

a31541cf9ebdbaabcbca35463fd2104d1e82ad68
Jody <qizhaodi@koderover.com>
2022-01-30 17:21:33 +0800

    style: partial detail style
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/common/task_list.vue
+++ src/common/task_list.vue
@@ -13,21 +13,44 @@
             {{ '#' +scope.row.task_id }}</router-link>
         </template>
       </el-table-column>
-      <el-table-column label="创建信息">
+      <el-table-column width="100"
+                       prop="status"
+                       label="运行状态">
         <template slot-scope="scope">
-          {{ convertTimestamp(scope.row.create_time)+' '+ scope.row.task_creator}}
+          <span :class="[`status-${$utils.taskElTagType(scope.row.status)}`]">
+            <i class="el-icon-orange"></i>
+            &nbsp;{{ wordTranslation(scope.row.status,'pipeline','task') }}
+          </span>
         </template>
       </el-table-column>
-      <el-table-column v-if="showEnv"
-                       width="100"
-                       label="集成环境">
+       <el-table-column width="120"
+                       label="持续时间">
         <template slot-scope="scope">
-          <span v-if="scope.row.workflow_args">
-            {{ scope.row.workflow_args.namespace}}
+          <el-icon name="time"></el-icon>
+          <span v-if="scope.row.status!=='running'"
+                style="margin-left: 5px;">
+            {{ $utils.timeFormatEn(scope.row.end_time - scope.row.start_time) }}
           </span>
-          <span v-else-if="scope.row.sub_tasks">
-            {{ getDeployEnv(scope.row.sub_tasks)}}
+          <span v-else
+                style="margin-left: 5px;">
+            {{ taskDuration(scope.row.task_id,scope.row.start_time) +
+              $utils.timeFormatEn(durationSet[scope.row.task_id]) }}
+            <el-tooltip v-if="durationSet[scope.row.task_id]<0"
+                        content="本地系统时间和服务端可能存在不一致，请同步。"
+                        placement="top">
+              <i class="el-icon-warning"
+                 style="color: red;"></i>
+            </el-tooltip>
           </span>
+        </template>
+      </el-table-column>
+      <el-table-column label="执行人">
+        <template slot-scope="{ row }">
+          <div style="color: #a0a0a0;">
+            <span>{{ row.task_creator }}</span>
+            <br>
+            <span>{{ convertTimestamp(row.create_time) }}</span>
+          </div>
         </template>
       </el-table-column>
       <el-table-column v-if="showServiceNames"
@@ -52,83 +75,73 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column width="120"
-                       label="持续时间">
+      <el-table-column width="230" label="代码信息">
+        <template slot-scope="{ row }">
+          <div v-for="(item,index) in getRepo(row)" :key="index">
+            <div v-if="item.builds.length > 0" class="item" effect="light"  :open-delay="250" placement="right">
+              <div slot="content">
+                <div v-for="(build,buildIndex) in item.builds" :key="buildIndex">
+                      <span v-if="build.commit_message">{{ build.commit_message.substring(0, 14) }}...</span>
+                      <span v-if="build.author_name"><i class="el-icon-orange"></i>{{ build.author_name }}</span>
+                      <!-- <span v-if="build.tag">Tag-{{build.tag }}</span>
+                      <span v-if="build.branch">Branch-{{ build.branch }}</span>
+                      <span v-if="build.pr">PR-{{ build.pr }}</span>
+                      <span v-if="build.commit_id">commit_id: {{build.commit_id.substring(0, 7)}}</span> -->
+                   </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column v-if="showEnv"
+                       width="100"
+                       label="集成环境">
         <template slot-scope="scope">
-          <el-icon name="time"></el-icon>
-          <span v-if="scope.row.status!=='running'"
-                style="margin-left: 5px; font-size: 13px;">
-            {{ $utils.timeFormat(scope.row.end_time - scope.row.start_time) }}
+          <span v-if="scope.row.workflow_args">
+            {{ scope.row.workflow_args.namespace}}
           </span>
-          <span v-else
-                style="margin-left: 5px; font-size: 13px;">
-            {{ taskDuration(scope.row.task_id,scope.row.start_time) +
-              $utils.timeFormat(durationSet[scope.row.task_id]) }}
-            <el-tooltip v-if="durationSet[scope.row.task_id]<0"
-                        content="本地系统时间和服务端可能存在不一致，请同步。"
-                        placement="top">
-              <i class="el-icon-warning"
-                 style="color: red;"></i>
-            </el-tooltip>
+          <span v-else-if="scope.row.sub_tasks">
+            {{ getDeployEnv(scope.row.sub_tasks)}}
           </span>
         </template>
       </el-table-column>
       <el-table-column v-if="showTestReport"
                        label="测试结果">
         <template slot-scope="scope">
-          <span v-if="scope.row.test_reports">
-            <el-popover v-for="(item,testIndex) in scope.row.testSummary"
-                        :key="testIndex+'function'"
-                        trigger="hover"
-                        placement="top"
-                        style="display: inline-block;">
-              <p><span>测试名称：{{item.name}}</span></p>
-              <p><span>总测试用例：{{item.total}}</span></p>
-              <p><span>成功用例：{{item.success}}</span></p>
-              <p><span>测试用时：{{$utils.timeFormat(parseInt(item.time))}}</span></p>
-              <div slot="reference">
-                <router-link v-if="item.type==='function'"
-                             :to="`${functionTestBaseUrl}/${scope.row.task_id}/test/${workflowName}-${scope.row.task_id}-test?is_workflow=1&service_name=${item.name}&test_type=${item.type}`">
-                  <span class="report-link"><i class="iconfont iconzidong"></i>
-                    {{item.success}}/{{item.total}}</span>
-                  <span v-if="testIndex!==(scope.row.testSummary.length - 1)">{{'&nbsp;'}}</span>
-                </router-link>
-              </div>
-            </el-popover>
-            <span v-for="(item,index) in scope.row.testSummary"
-                  :key="index+'performance'">
-              <router-link v-if="item.type==='performance'"
+          <template v-if="scope.row.test_reports">
+            <div v-for="(item,testIndex) in scope.row.testSummary"
+                 :key="testIndex">
+              <el-popover v-if="item.type==='function'"
+                          trigger="hover"
+                          placement="top">
+                <p><span>测试名称：{{item.name}}</span></p>
+                <p><span>总测试用例：{{item.total}}</span></p>
+                <p><span>成功用例：{{item.success}}</span></p>
+                <p><span>测试用时：{{$utils.timeFormat(parseInt(item.time))}}</span></p>
+                <div slot="reference">
+                  <router-link :to="`${functionTestBaseUrl}/${scope.row.task_id}/test/${workflowName}-${scope.row.task_id}-test?is_workflow=1&service_name=${item.name}&test_type=${item.type}`">
+                    <span class="report-link"><i class="iconfont iconzidong"></i>
+                      {{item.success}}/{{item.total}}</span>
+                  </router-link>
+                </div>
+              </el-popover>
+              <router-link v-else-if="item.type==='performance'"
+                           style="display: block;"
                            :to="`${functionTestBaseUrl}/${scope.row.task_id}/test/${workflowName}-${scope.row.task_id}-test?is_workflow=1&service_name=${item.name}&test_type=${item.type}`">
                 <span class="report-link">
                   <i class="iconfont iconxingneng"></i> 性能</span>
               </router-link>
-            </span>
-            <span v-for="(item,index) in scope.row.testSummary"
-                  :key="index+'security'">
-              <router-link v-if="item.type==='security'"
+              <router-link v-else-if="item.type==='security'"
+                           style="display: block;"
                            :to="`${securityTestBaseUrl}/${scope.row.task_id}?imageId=${item.img_id}`">
                 <span class="report-link"><i class="iconfont iconanquan"></i> 安全</span>
               </router-link>
-              <span v-if="index!==(scope.row.testSummary.length - 1)">{{'&nbsp;'}}</span>
-            </span>
-
-          </span>
+            </div>
+          </template>
           <span v-else>N/A</span>
         </template>
       </el-table-column>
 
-      <el-table-column width="120"
-                       prop="status"
-                       label="状态">
-        <template slot-scope="scope">
-          <el-tag :type="$utils.taskElTagType(scope.row.status)"
-                  size="small"
-                  effect="dark"
-                  close-transition>
-            {{ wordTranslation(scope.row.status,'pipeline','task') }}
-          </el-tag>
-        </template>
-      </el-table-column>
       <el-table-column v-if="showOperation"
                        width="180"
                        label="操作">

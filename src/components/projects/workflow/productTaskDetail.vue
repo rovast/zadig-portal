26a048024350dfc414b3760906969bd6fc512a28
leozhang2018 <leozhang2018@gmail.com>
2022-02-17 21:31:32 +0800

    fix(route):fix artifact's link.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/projects/workflow/productTaskDetail.vue
+++ src/components/projects/workflow/productTaskDetail.vue
@@ -49,7 +49,7 @@
             </el-form-item>
             <el-form-item v-if="versionList.length > 0 && taskDetail.status==='passed'"
                           label="交付清单">
-              <router-link :to="`/v1/delivery/version/${projectName}/${versionList[0].versionInfo.id}?deployType=k8s&version=${versionList[0].versionInfo.version}`">
+              <router-link :to="`/v1/delivery/version/detail/${projectName}/${versionList[0].versionInfo.id}?version=${versionList[0].versionInfo.version}&type=k8s`">
                 <span class="version-link">{{ $utils.tailCut(versionList[0].versionInfo.id,8,'#')+
             versionList[0].versionInfo.version }}</span>
               </router-link>

61127a0e09c24527b82d9a8f53a4594bf82c9e93
Jody <qizhaodi@koderover.com>
2022-02-24 20:15:54 +0800

    style(build): modify the style of the build list page
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/build/config.vue
+++ src/components/projects/build/config.vue
@@ -1,118 +1,79 @@
 <template>
   <div class="buildConfig-container">
     <!--start of edit service target dialog-->
-    <el-dialog title="修改服务"
-               custom-class="create-buildconfig"
-               :visible.sync="dialogEditTargetVisible">
-      <el-form label-position="left"
-               :model="editBuildManageTargets"
-               @submit.native.prevent
-               ref="editTargetForm">
+    <el-dialog title="修改服务" width="40%" custom-class="create-buildconfig" :visible.sync="dialogEditTargetVisible">
+      <el-form label-position="top" :model="editBuildManageTargets" @submit.native.prevent ref="editTargetForm">
         <el-form-item label="服务组件">
-          <el-select v-model="editBuildManageTargets.targets"
-                     value-key="key"
-                     multiple
-                     filterable>
-            <el-option v-for="(service,index) in serviceTargets"
-                       :key="index"
-                       :label="`${service.service_module}(${service.service_name})`"
-                       :value="service"></el-option>
+          <el-select v-model="editBuildManageTargets.targets" value-key="key" multiple filterable>
+            <el-option
+              v-for="(service,index) in serviceTargets"
+              :key="index"
+              :label="`${service.service_module}(${service.service_name})`"
+              :value="service"
+            ></el-option>
           </el-select>
         </el-form-item>
       </el-form>
-      <div slot="footer"
-           class="dialog-footer">
-        <el-button type="primary"
-                   native-type="submit"
-                   @click="saveServiceTarget()"
-                   class="start-create">确定</el-button>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" native-type="submit" @click="saveServiceTarget()" class="start-create">确定</el-button>
       </div>
     </el-dialog>
     <!--end of edit service target dialog-->
-    <div class="tab-container">
-      <div>
-        <el-alert type="info"
-                  :closable="false"
-                  description="定义服务组件的构建方式以及构建依赖">
-        </el-alert>
-        <div class="create-buildconfig">
-          <el-button v-if="!searchInputVisible"
-                     @click="searchInputVisible=true"
-                     plain
-                     size="small"
-                     type="primary"
-                     icon="el-icon-search"></el-button>
-          <transition name="fade">
-            <el-input v-if="searchInputVisible"
-                      size="small"
-                      v-model.lazy="searchBuildConfig"
-                      placeholder="请输入构建名称"
-                      style="width: auto;"
-                      autofocus
-                      prefix-icon="el-icon-search">
-            </el-input>
-          </transition>
-
-        </div>
-        <div class="config-container">
-          <el-table :data="filteredBuildConfigs"
-                    style="width: 100%;">
-            <el-table-column label="名称">
-              <template slot-scope="scope">
-                {{ scope.row.name }}
-              </template>
-            </el-table-column>
-            <el-table-column prop="services"
-                             label="服务组件">
-              <template slot-scope="scope">
-                <template v-if="scope.row.targets.length > 0">
-                  <div v-for="(item,index) in scope.row.targets"
-                       :key="index">
-                    <el-tooltip effect="dark"
-                                :content="item.service_name"
-                                placement="top">
-                      <span>{{`${item.service_module}`}}</span>
-                    </el-tooltip>
-                  </div>
-                </template>
-                <span v-hasPermi="{projectName: projectName, action: 'edit_build'}" class="change-serviceTarget"><i @click="changeServiceTargets(scope.row)"
-                     class="el-icon-edit"></i></span>
-              </template>
-            </el-table-column>
-            <el-table-column label="更新时间">
-              <template slot-scope="scope">
-                {{ $utils.convertTimestamp(scope.row.update_time) }}
-              </template>
-            </el-table-column>
-            <el-table-column label="最后修改">
-              <template slot-scope="scope">
-                {{scope.row.update_by }}
-              </template>
-            </el-table-column>
-            <el-table-column label="操作"
-                             width="240">
-              <template slot-scope="scope">
-                <router-link v-hasPermi="{projectName: projectName, action: 'edit_build'}"
-                             :to="`/v1/projects/detail/${scope.row.productName}/builds/detail/${scope.row.name}`">
-                  <el-button type="primary"
-                             size="mini"
-                             plain>编辑</el-button>
-                </router-link>
-                <el-button v-hasPermi="{projectName: projectName, action: 'delete_build'}" @click="removeBuildConfig(scope.row)"
-                           type="danger"
-                           size="mini"
-                           plain>删除</el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-        </div>
+    <section class="tab-container">
+      <div class="build-search-container">
+        <el-input v-model.lazy="searchBuildConfig" placeholder="请输入构建名称" class="search-test" autofocus prefix-icon="el-icon-search"></el-input>
       </div>
-    </div>
+      <el-table :data="filteredBuildConfigs">
+        <el-table-column label="名称" prop="name" min-width="180"></el-table-column>
+        <el-table-column prop="services" label="服务组件" min-width="180">
+          <template slot-scope="scope">
+            <div class="service-container">
+              <div v-if="scope.row.targets.length > 0" class="service-left">
+                <div v-for="(item,index) in scope.row.targets" :key="index">
+                  <el-tooltip effect="dark" :content="item.service_name" placement="top">
+                    <span>{{ item.service_module }}</span>
+                  </el-tooltip>
+                </div>
+              </div>
+              <div v-hasPermi="{projectName: projectName, action: 'edit_build'}" class="change-serviceTarget">
+                <i @click="changeServiceTargets(scope.row)" class="el-icon-edit-outline"></i>
+              </div>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column label="更新时间" width="180">
+          <template slot-scope="scope">{{ $utils.convertTimestamp(scope.row.update_time) }}</template>
+        </el-table-column>
+        <el-table-column label="最后修改" prop="update_by" width="180"></el-table-column>
+        <el-table-column label="操作" width="180">
+          <template slot-scope="scope">
+            <router-link
+              v-hasPermi="{projectName: projectName, action: 'edit_build'}"
+              :to="`/v1/projects/detail/${scope.row.productName}/builds/detail/${scope.row.name}`"
+            >
+              <el-button type="primary" size="mini" plain>编辑</el-button>
+            </router-link>
+            <el-button
+              v-hasPermi="{projectName: projectName, action: 'delete_build'}"
+              @click="removeBuildConfig(scope.row)"
+              type="danger"
+              size="mini"
+              plain
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </section>
   </div>
 </template>
 
 <script>
-import { getBuildConfigsAPI, deleteBuildConfigAPI, saveBuildConfigTargetsAPI, getServiceTargetsAPI } from '@api'
+import {
+  getBuildConfigsAPI,
+  deleteBuildConfigAPI,
+  saveBuildConfigTargetsAPI,
+  getServiceTargetsAPI
+} from '@api'
 import { flattenDeep } from 'lodash'
 import bus from '@utils/eventBus'
 export default {
@@ -136,7 +97,7 @@ export default {
         existServices.push(element.targets)
       })
       return services.filter(element => {
-        if (!(flattenDeep(existServices).includes(element.service_name))) {
+        if (!flattenDeep(existServices).includes(element.service_name)) {
           return element
         }
         return false
@@ -144,9 +105,10 @@ export default {
     },
     removeBuildConfig (obj) {
       const projectName = this.projectName
-      const str = obj.pipelines.length > 0
-        ? `该配置在 ${obj.pipelines} 存在引用，确定要删除 ${obj.name} 吗？`
-        : `确定要删除 ${obj.name} 吗？`
+      const str =
+        obj.pipelines.length > 0
+          ? `该配置在 ${obj.pipelines} 存在引用，确定要删除 ${obj.name} 吗？`
+          : `确定要删除 ${obj.name} 吗？`
       this.$confirm(str, '确认', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
@@ -160,8 +122,11 @@ export default {
     },
     changeServiceTargets (scope) {
       const projectName = this.projectName
-      getServiceTargetsAPI(projectName).then((data) => {
-        this.serviceTargets = [...this.filterAvailableServices(data), ...scope.targets].map(element => {
+      getServiceTargetsAPI(projectName).then(data => {
+        this.serviceTargets = [
+          ...this.filterAvailableServices(data),
+          ...scope.targets
+        ].map(element => {
           element.key = element.service_name + '/' + element.service_module
           return element
         })
@@ -187,7 +152,7 @@ export default {
     },
     getBuildConfig () {
       const projectName = this.projectName
-      getBuildConfigsAPI(projectName).then((res) => {
+      getBuildConfigsAPI(projectName).then(res => {
         res.forEach(element => {
           element.targets = element.targets.map(t => {
             t.key = t.service_name + '/' + t.service_module
@@ -207,7 +172,11 @@ export default {
           }
         })
       }
-      return this.$utils.filterObjectArrayByKey('name', this.searchBuildConfig, this.buildConfigs)
+      return this.$utils.filterObjectArrayByKey(
+        'name',
+        this.searchBuildConfig,
+        this.buildConfigs
+      )
     },
     projectName () {
       return this.$route.params.project_name
@@ -219,46 +188,26 @@ export default {
       this.searchInputVisible = true
       this.searchBuildConfig = this.$route.query.name
     } else if (this.$route.query.add) {
-      this.$router.replace(`/v1/projects/detail/${this.projectName}/builds/create`)
+      this.$router.replace(
+        `/v1/projects/detail/${this.projectName}/builds/create`
+      )
     }
-    bus.$emit('set-topbar-title', { title: '', breadcrumb: [{ title: '项目', url: '/v1/projects' }, { title: this.projectName, url: `/v1/projects/detail/${this.projectName}/detail` }, { title: '构建', url: '' }] })
+    bus.$emit('set-topbar-title', {
+      title: '',
+      breadcrumb: [
+        { title: '项目', url: '/v1/projects' },
+        {
+          title: this.projectName,
+          url: `/v1/projects/detail/${this.projectName}/detail`
+        },
+        { title: '构建', url: '' }
+      ]
+    })
   }
 }
 </script>
 
-<style lang="less">
-.create-buildconfig {
-  width: 400px;
-
-  .el-dialog__header {
-    padding: 15px;
-    text-align: center;
-    border-bottom: 1px solid #e4e4e4;
-
-    .el-dialog__close {
-      font-size: 10px;
-    }
-  }
-
-  .el-dialog__body {
-    padding-bottom: 0;
-  }
-
-  .dialog-footer {
-    .start-create {
-      width: 100%;
-    }
-  }
-
-  .el-select {
-    width: 100%;
-  }
-
-  .el-input {
-    display: inline-block;
-  }
-}
-
+<style lang="less" scoped>
 .buildConfig-container {
   position: relative;
   flex: 1;
@@ -266,54 +215,44 @@ export default {
   padding: 15px 20px;
   overflow: auto;
 
-  .breadcrumb {
-    margin-bottom: 25px;
+  .create-buildconfig {
+    width: 400px;
 
-    .el-breadcrumb {
-      font-size: 16px;
-      line-height: 1.35;
-
-      .el-breadcrumb__item__inner a:hover,
-      .el-breadcrumb__item__inner:hover {
-        color: @themeColor;
-        cursor: pointer;
+    .dialog-footer {
+      .start-create {
+        width: 100%;
       }
+    }
+
+    .el-select {
+      width: 100%;
     }
   }
 
-  .create-buildconfig {
-    margin-top: 25px;
-    margin-bottom: 15px;
+  .build-search-container {
+    height: 40px;
+    margin-bottom: 10px;
+    text-align: right;
+
+    .search-test {
+      width: 400px;
+    }
   }
 
-  .change-serviceTarget {
-    color: @themeColor;
-    cursor: pointer;
-  }
+  .service-container {
+    display: flex;
+    align-items: center;
 
-  .el-table th > .cell {
-    color: #97a8be;
-  }
+    .service-left {
+      width: 180px;
+    }
 
-  .el-input {
-    display: inline-block;
-  }
-}
-
-.deltpt-dialog {
-  .el-dialog__footer {
-    box-sizing: border-box;
-    padding: 10px 20px 15px;
-    text-align: center;
-  }
-
-  .el-dialog__header,
-  .el-dialog__body {
-    text-align: center;
-  }
-
-  .el-input {
-    display: inline-block;
+    .change-serviceTarget {
+      margin-left: 20px;
+      color: @themeColor;
+      font-size: 16px;
+      cursor: pointer;
+    }
   }
 }
 </style>

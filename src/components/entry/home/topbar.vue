bf439cf3fe4cee2ca4d6357e9fb6cfe6949dd7f3
Jody <qizhaodi@koderover.com>
2022-03-01 22:17:45 +0800

    feat: add Document Station
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/entry/home/topbar.vue
+++ src/components/entry/home/topbar.vue
@@ -42,6 +42,75 @@
         </div>
       </div>
       <div class="topbar-container-end">
+        <el-popover placement="bottom" popper-class="help-droplist" trigger="click">
+          <ul class="dropdown-menu" uib-dropdown-menu>
+            <li>
+              <a href="https://docs.koderover.com/zadig/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>文档站</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://docs.koderover.com/zadig/quick-start/introduction/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>入门</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.koderover.com/tutorials/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>最佳实践</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://docs.koderover.com/zadig/workflow/trigger/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>工作流</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://docs.koderover.com/zadig/env/service/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>集成环境</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://docs.koderover.com/zadig/project/service/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>项目管理</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://docs.koderover.com/zadig/delivery/artifact/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>交付中心</span>
+              </a>
+            </li>
+            <li role="separator" class="divider"></li>
+            <li>
+              <a href="https://docs.koderover.com/zadig/settings/codehost/gitlab/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>系统设置</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://docs.koderover.com/zadig/api/usage/" target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>开发者中心</span>
+              </a>
+            </li>
+            <!-- <li>
+              <a href="https://docs.koderover.com/zadig/glossary/"
+                 target="_blank">
+                <i class="icon el-icon-link"></i>
+                <span>术语表</span>
+              </a>
+            </li>-->
+          </ul>
+          <span slot="reference">
+            <i class="help-icon iconfont iconvery-teach"></i>
+          </span>
+        </el-popover>
         <span>
           <Notification class="icon notify" />
         </span>
@@ -208,49 +277,55 @@ export default {
   }
 }
 
-// .help-droplist {
-//   .dropdown-menu {
-//     margin: 0 2px;
-//     padding: 8px 0;
-//     list-style: none;
+.help-droplist {
+  .dropdown-menu {
+    margin: 0 2px;
+    padding: 8px 0;
+    list-style: none;
 
-//     .divider {
-//       height: 1px;
-//       margin: 9px 0;
-//       overflow: hidden;
-//       background-color: #e5e5e5;
-//     }
+    .divider {
+      height: 1px;
+      margin: 9px 0;
+      overflow: hidden;
+      background-color: #e5e5e5;
+    }
 
-//     li {
-//       & > a {
-//         display: block;
-//         clear: both;
-//         padding: 3px 20px;
-//         padding: 4px 0 4px 5px;
-//         color: #333;
-//         font-weight: normal;
-//         line-height: 1.42857143;
-//         white-space: nowrap;
+    li {
+      & > a {
+        display: block;
+        clear: both;
+        padding: 3px 20px;
+        padding: 4px 0 4px 5px;
+        color: #333;
+        font-weight: normal;
+        line-height: 1.42857143;
+        white-space: nowrap;
 
-//         .icon {
-//           position: relative;
-//           margin-right: 5px;
-//           font-size: 16px;
-//         }
-//       }
+        .icon {
+          position: relative;
+          margin-right: 5px;
+          font-size: 16px;
+        }
+      }
 
-//       &:hover {
-//         color: #262626;
-//         text-decoration: none;
-//         background-color: #f5f5f5;
+      &:hover {
+        color: #262626;
+        text-decoration: none;
+        background-color: #f5f5f5;
 
-//         & > a {
-//           color: @themeColor;
-//         }
-//       }
-//     }
-//   }
-// }
+        & > a {
+          color: @themeColor;
+        }
+      }
+    }
+  }
+}
+
+.help-icon {
+  margin: 0 38px;
+  color: #a0a0a0;
+  font-size: 28px;
+}
 
 .flex {
   display: flex;

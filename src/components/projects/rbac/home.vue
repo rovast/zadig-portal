6af0bfbea2ba03fd82b8cccc7109fe836a536e31
Jody <qizhaodi@koderover.com>
2022-03-02 17:15:54 +0800

    fix: xix permission page can't scroll
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/rbac/home.vue
+++ src/components/projects/rbac/home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="project-rbac-container">
-    <div class="tab-container" style="width: 100%;">
-      <el-tabs  v-model="activeTab" :tab-position="tabPosition" type="card" style="height: 200px;">
-        <el-tab-pane name="member" label="成员管理"><Member v-if="activeTab==='member'" :projectName="projectName" /></el-tab-pane>
-        <el-tab-pane name="role" label="角色管理"><Role  v-if="activeTab==='role'" :projectName="projectName"/></el-tab-pane>
-        <el-tab-pane name="policy" label="策略管理">
-          <Policy v-if="activeTab==='policy'" :projectName="projectName"></Policy>
-        </el-tab-pane>
-      </el-tabs>
-    </div>
+    <el-tabs v-model="activeTab" :tab-position="tabPosition" type="card" class="permission-tabs">
+      <el-tab-pane name="member" label="成员管理">
+        <Member v-if="activeTab==='member'" :projectName="projectName" />
+      </el-tab-pane>
+      <el-tab-pane name="role" label="角色管理">
+        <Role v-if="activeTab==='role'" :projectName="projectName" />
+      </el-tab-pane>
+      <el-tab-pane name="policy" label="策略管理">
+        <Policy v-if="activeTab==='policy'" :projectName="projectName"></Policy>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script>
@@ -27,7 +29,6 @@ export default {
     return {
       activeTab: 'member',
       tabPosition: 'top'
-
     }
   },
   methods: {},
@@ -41,7 +42,19 @@ export default {
 
 <style lang="less" scoped>
 .project-rbac-container {
+  box-sizing: border-box;
+  height: 100%;
   padding: 25px 20px;
   font-size: 15px;
+
+  /deep/.el-tabs.permission-tabs {
+    height: 100%;
+
+    .el-tabs__content {
+      height: calc(~'100% - 70px');
+      margin-bottom: 10px;
+      overflow: auto;
+    }
+  }
 }
 </style>

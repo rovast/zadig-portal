79f73bb59f0f240683ecd1c71e725f2ab8a263b5
Jody <qizhaodi@koderover.com>
2022-03-08 19:54:45 +0800

    fix: add a link to create a new template
    Signed-off-by: Jody <qizhaodi@koderover.com>

--- src/components/projects/serviceMgr/common/importFromTemplate.vue
+++ src/components/projects/serviceMgr/common/importFromTemplate.vue
@@ -14,8 +14,14 @@
         <el-form-item label="服务名称" prop="serviceName" :rules="{ required: true, message: '服务名称不能为空', trigger: ['change','blur'] }">
           <el-input style="width: 400px;" v-model.trim="importYaml.serviceName" size="small" placeholder="请输入服务名称" clearable></el-input>
         </el-form-item>
-        <el-form-item label="模板选择" prop="id" :rules="{ required: true, message: '请选择模板', trigger: ['change','blur'] }">
+        <el-form-item label="选择模板" prop="id" :rules="{ required: true, message: '请选择模板', trigger: ['change','blur'] }">
           <el-select style="width: 400px;" size="small" v-model="importYaml.id" placeholder="请选择模板" @change="getKubernetesTemplate">
+            <el-option disabled value="NEWMODULE">
+              <router-link to="/v1/template/k8s-yamls" class="module-link">
+                <i class="el-icon-circle-plus-outline" style="margin-right: 3px;"></i>
+                新建模板
+              </router-link>
+            </el-option>
             <el-option v-for="item in importYaml.yamls" :key="item.id" :label="item.name" :value="item.id"></el-option>
           </el-select>
         </el-form-item>
@@ -160,46 +166,54 @@ export default {
 }
 </script>
 <style lang="less">
-  .dialog-source,
-  .dialog-import-from-template {
-    .el-dialog__header {
-      padding: 20px 20px 10px;
-      font-size: 16px;
-      border: 1px solid #d2d2d2;
-    }
+.dialog-source,
+.dialog-import-from-template {
+  .el-dialog__header {
+    padding: 20px 20px 10px;
+    font-size: 16px;
+    border: 1px solid #d2d2d2;
+  }
 
-    .el-dialog__footer {
-      padding: 0 20px 20px;
-      text-align: right;
-    }
+  .el-dialog__footer {
+    padding: 0 20px 20px;
+    text-align: right;
+  }
 
-    .el-dialog__body {
-      padding: 40px 106px 0;
+  .el-dialog__body {
+    padding: 40px 106px 0;
 
-      .preload-container {
-        .service-name,
-        .contains {
-          color: #909399;
-          font-size: 13px;
-        }
+    .preload-container {
+      .service-name,
+      .contains {
+        color: #909399;
+        font-size: 13px;
+      }
 
-        .service-name {
-          display: inline-block;
-          margin-right: 4px;
-          color: #303133;
-        }
+      .service-name {
+        display: inline-block;
+        margin-right: 4px;
+        color: #303133;
       }
     }
-
-    .preload-error {
-      color: #ff1949;
-      font-size: 12px;
-    }
-
-    .el-table th.el-table__cell {
-      padding: 0;
-      color: #888;
-      background: #fff;
-    }
   }
+
+  .preload-error {
+    color: #ff1949;
+    font-size: 12px;
+  }
+
+  .el-table th.el-table__cell {
+    padding: 0;
+    color: #888;
+    background: #fff;
+  }
+}
+
+.module-link {
+  display: inline-block;
+  width: 100%;
+  height: calc(~'100% - 1px');
+  color: #606266;
+  border-bottom: 1px solid #d2d7dc;
+}
 </style>

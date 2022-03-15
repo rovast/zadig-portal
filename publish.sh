54e8ebd77991f72b0286fefa3f0a94f4365c4cea
leozhang2018 <leozhang2018@gmail.com>
2022-03-15 10:15:53 +0800

    chore:update build script to support buildx
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- publish.sh
+++ publish.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-## Build img
+## Build docker image.
 
 version=$1
 
+arch_name="$(uname -m)"
+
 if [ $# = 0 ];then
-    echo "version must provide. example: ./publish.sh 1.0.0"
+    echo "Version must be provided. example: ./publish.sh 1.0.0"
     exit 1
 fi
-
+ 
 yarn install --ignore-engines
 
 yarn run build
 
-docker build --rm=true -t ccr.ccs.tencentyun.com/koderover-rc/zadig-portal:$version -f amd64.Dockerfile .
-
-docker push ccr.ccs.tencentyun.com/koderover-rc/zadig-portal:$version
-
-
+if [ "${arch_name}" = "x86_64" ]; then
+    docker build --rm=true -t ccr.ccs.tencentyun.com/koderover-rc/zadig-portal:$version -f amd64.Dockerfile .
+    docker push ccr.ccs.tencentyun.com/koderover-rc/zadig-portal:$version
+elif [ "${arch_name}" = "arm64" ]; then
+    docker buildx build --platform linux/amd64 --rm=true -t ccr.ccs.tencentyun.com/koderover-rc/zadig-portal:$version -f amd64.Dockerfile .
+    docker push ccr.ccs.tencentyun.com/koderover-rc/zadig-portal:$version
+else
+    echo "Unknown architecture: ${arch_name}"
+    exit 1
+fi
 
 

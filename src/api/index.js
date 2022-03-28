7c3c0d0bb8f064f9fd6de3bf9f09bcea2f32114c
liyue326 <liyue@koderover.com>
2022-03-28 20:29:52 +0800

    fix: core scene problems
    Signed-off-by: liyue326 <liyue@koderover.com>

--- src/api/index.js
+++ src/api/index.js
@@ -593,9 +593,9 @@ export function getBuildTargetsAPI (projectName) {
 
 export function runWorkflowAPI (projectName, data, isArtifact = false) {
   if (isArtifact) {
-    return http.put(`/api/aslan/workflow/workflowtask?projectName=${projectName}`, data)
+    return http.put(`/api/aslan/workflow/workflowtask/${data.workflow_name}?projectName=${projectName}`, data)
   } else {
-    return http.post(`/api/aslan/workflow/workflowtask?projectName=${projectName}`, data)
+    return http.post(`/api/aslan/workflow/workflowtask/${data.workflow_name}?projectName=${projectName}`, data)
   }
 }
 

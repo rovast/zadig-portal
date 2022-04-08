20c5e8cc88d40a9319face6ad0903844d849c017
leozhang2018 <leozhang2018@gmail.com>
2022-04-08 11:54:22 +0800

    fix(typo):fix typo.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- src/components/projects/common/importValues/repository.vue
+++ src/components/projects/common/importValues/repository.vue
@@ -95,7 +95,7 @@
       <el-form-item prop="autoSync" label="自动同步">
         <span slot="label">
           <span>自动同步</span>
-          <el-tooltip effect="dark" content="开启后，Zadig 会定时从代码库拉取 Values 文件并将其自动更新到环境中" placement="top">
+          <el-tooltip effect="dark" content="开启后，Zadig 会定时从代码库拉取 Values 文件并将其自动更新到环境中，目前只支持 GitHub/GitLab" placement="top">
             <i class="pointer el-icon-question"></i>
           </el-tooltip>
         </span>

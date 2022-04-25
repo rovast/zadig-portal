0a746802f9f277afc92b4c9f6ce8b92ce5a53183
liyue326 <liyue@koderover.com>
2022-04-25 20:45:16 +0800

    fix: remove backEndAddr
    Signed-off-by: liyue326 <liyue@koderover.com>

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path');
-const backEndAddr = 'http://dev3.8slan.com/';
+const backEndAddr = '';
 
 module.exports = {
   dev: {

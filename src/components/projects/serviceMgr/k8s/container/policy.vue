0da8542422302def14fa377bd57846e8faed560e
liyue326 <liyue@koderover.com>
2022-05-05 16:54:40 +0800

    feat: auto deploy
    Signed-off-by: liyue326 <liyue@koderover.com>

--- src/components/projects/serviceMgr/k8s/container/policy.vue
+++ src/components/projects/serviceMgr/k8s/container/policy.vue
@@ -5,6 +5,16 @@
         <el-form-item label="服务部署超时设置（分钟）" prop="timeout">
           <el-input v-model.number="projectForm.timeout"></el-input>
         </el-form-item>
+        <el-form-item prop="auto_deploy.auto_sync" v-if="showAutoUpdate">
+          <span slot="label">
+            <span>服务自动更新</span>
+            <el-tooltip effect="dark" content="开启自动更新后，服务配置变更时，Zadig 会自动将其部署到项目中所有环境中" placement="top">
+              <i class="pointer el-icon-warning"></i>
+            </el-tooltip>
+          </span>
+          <span>开启自动更新</span>
+          <el-switch v-model="projectForm.auto_deploy.auto_sync" />
+        </el-form-item>
         <el-form-item label="交付物命名规则设置">
           <span slot="label">
             交付物命名规则设置
@@ -80,7 +90,10 @@ export default {
     return {
       projectForm: {
         timeout: null,
-        custom_image_rule: {}
+        custom_image_rule: {},
+        auto_deploy: {
+          auto_sync: false
+        }
       },
       rules: {
         timeout: [
@@ -96,6 +109,18 @@ export default {
   computed: {
     projectName () {
       return this.$route.params.project_name
+    },
+    showAutoUpdate () {
+      const project = this.$store.getters.projectList.find(
+        project => project.name === this.projectName
+      )
+      if (!this.projectName) {
+        return false
+      } else if (project && (project.deployType === 'helm' || project.deployType === 'k8s')) {
+        return true
+      } else {
+        return false
+      }
     }
   },
   methods: {
@@ -104,6 +129,9 @@ export default {
         this.projectForm = res
         if (!res.timeout) {
           this.$set(this.projectForm, 'timeout', 10)
+        }
+        if (!res.auto_deploy) {
+          this.$set(this.projectForm, 'auto_deploy', { auto_sync: false })
         }
       })
     },
